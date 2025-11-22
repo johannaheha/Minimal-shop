95ba54b782525e2e582e8a1996201d5e05be7341
Johanna Hammerl <johanna_hammerl@t-online.de>
2025-11-22 15:32:15 +0100

    implement product CRUD with UUID and ImgUrl

---
+++ backend/src/products/product.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @Column('float')
+  price: number;
+
+  @Column({ nullable: true })
+  imageUrl?: string;
+}
