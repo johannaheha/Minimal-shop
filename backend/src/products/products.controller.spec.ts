371300fe7f70d7a70fa14f689d522a2c4e88c075
Johanna Hammerl <johanna_hammerl@t-online.de>
2025-11-22 16:34:07 +0100

    fix products service und controller tests with mocked repository

--- backend/src/products/products.controller.spec.ts
+++ backend/src/products/products.controller.spec.ts
@@ -1,5 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Product } from './product.entity';
+
+const mockProductRepository = {
+  create: jest.fn(),
+  save: jest.fn(),
+  find: jest.fn(),
+  findOne: jest.fn(),
+  remove: jest.fn(),
+};
 
 describe('ProductsController', () => {
   let controller: ProductsController;
@@ -7,6 +18,13 @@ describe('ProductsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductsController],
+      providers: [
+        ProductsService,
+        {
+          provide: getRepositoryToken(Product),
+          useValue: mockProductRepository,
+        },
+      ],
     }).compile();
 
     controller = module.get<ProductsController>(ProductsController);
--- backend/src/products/products.service.spec.ts
+++ backend/src/products/products.service.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsService } from './products.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Product } from './product.entity';
 
 describe('ProductsService', () => {
   let service: ProductsService;
 
+  const mockProductRepository = {
+    create: jest.fn(),
+    save: jest.fn(),
+    find: jest.fn(),
+    findOne: jest.fn(),
+    remove: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductsService],
+      providers: [
+        ProductsService,
+        {
+          provide: getRepositoryToken(Product),
+          useValue: mockProductRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<ProductsService>(ProductsService);
